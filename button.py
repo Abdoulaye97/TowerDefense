eed7dc15499207e6202ac2fe7d60eee3aaaee5a1
Abdoulaye Diaw <diawabdel00@gmail.com>
2023-03-14 12:23:36 +0000

    update

--- button.py
+++ button.py
@@ -1,6 +1,10 @@
 import pygame
 
 
+def Quitt():
+    return False
+
+
 class Button:
     # le constrcteur pour creer un bouton Menu
     def __init__(self, x, y, image_path):
@@ -11,16 +15,13 @@ class Button:
         self.menu_optons = pygame.image.load(image_path)
 
     # cet fonction permet de verifier si notre button est cliquer
-    def is_clicked(self, mouse_pos):
+    def is_clicked(self, position):
         # verifie si le bouton et le souris sont au meme coordonnes au clik
-        if self.rect.collidepoint(mouse_pos):
+        if self.rect.collidepoint(position):
             self.start = True
             return True
         else:
             return False
-
-    def Quitt(self):
-        return False
 
     def Options(self):
         return self.image_options
